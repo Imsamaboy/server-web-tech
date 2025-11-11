6f0469591a2a1b463ce900be5cd327fbdee65954
Stanislav <55299328+Imsamaboy@users.noreply.github.com>
2025-11-11 20:28:47 +0300

    Task 6 solution

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ const fs = require('fs');
 
 const app = express();
 const upload = multer(); // сохраняем в оперативной памяти
+const SYSTEM_LOGIN = "";
+const TEXT_PLAIN_HEADER = { "Content-Type": "text/plain; charset=utf-8" };
 
-const LOGIN = "sfelshtyn"; // заменить login
+const LOGIN = "99803203-b584-4d0c-a62e-0e9704ea6563"; // заменить login
 
 app.get('/login', (req, res) => {
     res.type('text/plain').send(LOGIN);
@@ -15,21 +17,21 @@ app.get('/login', (req, res) => {
 
 // === Маршрут /size2json ===
 // Получает PNG изображение в поле image (multipart/form-data)
-app.post("/size2json", upload.single("image"), async (req, res) => {
+app.post("/insert/", async (req, res) => {
+    const { login, password, URL: mongoUrl } = req.body;
+    if (!login || !password || !mongoUrl) return res.status(400).set(TEXT_PLAIN_HEADER).send("Error: 'login', 'password', and 'URL' are required in the body.");
+    const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
     try {
-        if (!req.file) {
-            return res.status(400).json({ error: "Не передано поле image" });
-        }
-
-        // Используем sharp для получения размеров изображения
-        const metadata = await sharp(req.file.buffer).metadata();
-
-        res.json({
-            width: metadata.width,
-            height: metadata.height
-        });
+        await client.connect();
+        const db = client.db();
+        const collection = db.collection("users");
+        const doc = { login, password };
+        await collection.insertOne(doc);
+        res.set(TEXT_PLAIN_HEADER).status(201).send("User created successfully.");
     } catch (err) {
-        res.status(500).json({ error: "Ошибка обработки изображения" });
+        res.status(500).set(TEXT_PLAIN_HEADER).send(err.toString());
+    } finally {
+        if (client) await client.close();
     }
 });
 
