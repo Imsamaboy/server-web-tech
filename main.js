c1445d058bb88cdbe6e8990df25b4b5a5e8cfdd1
Stanislav <55299328+Imsamaboy@users.noreply.github.com>
2025-11-25 21:47:17 +0300

    Task 6 solution

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const pug = require('pug');
 const https = require('https');
 const fs = require('fs');
 
-const login = '99803203-b584-4d0c-a62e-0e9704ea6563';
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
@@ -16,37 +20,35 @@ app.use((req, res, next) => {
 });
 
 app.get('/login/', (_, res) => {
-  res.send(login);
+  res.send('99803203-b584-4d0c-a62e-0e9704ea6563');
 });
 
-app.get('/wordpress/wp-json/wp/v2/posts/1', (_, res) => {
-  res.json({
-    id: 1,
-    slug: login,
-    title: {
-      rendered: login
-    },
-    content: {
-      rendered: "",
-      protected: false
-    }
+app.get('/test/', async (req, res) => {
+  const targetURL = req.query.URL;
+
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    executablePath: '/usr/bin/chromium-browser', // Путь до chromium в системе
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // Эта строка добавлена для моего VPS на Ubuntu 24.04
+  })
+
+  const page = await browser.newPage();
+  await page.goto(targetURL, { waitUntil: 'networkidle2' });
+
+  await page.click('#bt');
+
+  await page.waitForFunction(() => {
+    const input = document.querySelector('#inp');
+    return input.value;
+  }, { timeout: 1000 });
+
+  const result = await page.evaluate(() => {
+    return document.querySelector('#inp').value;
   });
-});
 
-app.use(express.json());
+  await browser.close();
 
-app.post('/render/', async (req, res) => {
-  const { random2, random3 } = req.body;
-  const { addr } = req.query;
-
-  const templateResponse = await axios.get(addr);
-  const pugTemplate = templateResponse.data;
-
-  const compiled = pug.compile(pugTemplate);
-  const html = compiled({ random2, random3 });
-
-  res.set('Content-Type', 'text/html');
-  res.send(html);
+  res.send(result);
 });
 
 const PORT = process.env.PORT || 3000;
